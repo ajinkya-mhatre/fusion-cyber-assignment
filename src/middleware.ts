71603910cb090467bb963f0ea507ccb6156498ca
Ajinkya Mhatre <ajinkya.mhatre.b@gmail.com>
2024-03-24 16:25:41 +0530

    WIP

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,9 @@ import { NextResponse } from "next/server";
 export function middleware(request: NextRequest) {
   const token = request.cookies.get("token")?.value;
   if (!token) {
-    return NextResponse.redirect(`/auth/login`);
+    return NextResponse.redirect(
+      `${process.env.NEXT_PUBLIC_APP_HOST}/auth/login`,
+    );
   }
 }
 
